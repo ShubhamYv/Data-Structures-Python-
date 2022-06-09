e09058b966fe55804afe72cf4fd9a61e26eacf74
Shubham Kumar Yadav <75082349+ShubhamYv@users.noreply.github.com>
2022-06-09 20:12:15 +0530

    Update 2. Binary Search.py

--- Searching/2. Binary Search.py
+++ Searching/2. Binary Search.py
@@ -1,3 +1,5 @@
+"""ITERATIVE"""
+
 def binarySearch(arr, x):
     n= len(arr)
     low= 0
@@ -14,11 +16,27 @@ def binarySearch(arr, x):
         
     
     
+"""RECURSIVE"""    
+    
+# def binarySearch(arr,low, high, x):
+#     while low> high:
+#         return -1
+#     mid= low+ (high-low)//2
+#     if arr[mid]== x:
+#         return mid
+#     elif arr[mid]> x:
+#         return binarySearch(arr, low, mid-1, x)
+#     else:
+#         return binarySearch(arr, mid+1, high, x)
+    
+    
 def main():
     n= int(input())
     arr= list(map(int, input().split()))
     x= int(input())
-    res= binarySearch(arr, x)
+    res= binarySearch(arr, x) #FOR ITERATIVE
+#     res= binarySearch(arr, 0, n-1, x)  #For RECURSIVE
+    
     if res== -1:
         print("Element not present...")
     else:
