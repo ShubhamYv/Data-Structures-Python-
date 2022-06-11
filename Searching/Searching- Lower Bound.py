6a5095fc534960d66e50aba5174053d63fbc121f
Shubham Kumar Yadav <75082349+ShubhamYv@users.noreply.github.com>
2022-06-11 23:17:05 +0530

    Add files via upload

---
+++ Searching/Searching- Lower Bound.py
@@ -0,0 +1,22 @@
+def lowerBound(arr, n, ele):
+    low= 0
+    high= n
+    while low< high:
+        mid= (low+high)//2
+        if arr[mid]>= ele:
+            high= mid
+        else:
+            low= mid+1
+    return low
+
+def main():
+    t= int(input())
+    while t>0:
+        n= int(input())
+        arr= list(map(int, input().split()))
+        ele= int(input())
+        print(lowerBound(arr, n, ele))
+        t-=1
+
+if __name__ == '__main__':
+    main()
